29a131953c7d9a566417721ce6a15a102435b680
Sandesh Choudhary <sandeshchoudhary5@gmail.com>
2020-07-13 10:15:29 +0530

    Updates

---
+++ precache-manifest.c48bf4cf68dfcc12f9ea271c847076dc.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54d64d894e91a17c0ffe80695a3973eb",
+    "url": "/ds-cards/editor.worker.js"
+  },
+  {
+    "revision": "c01e5326c4ce26d44abb8d2925ca2aef",
+    "url": "/ds-cards/index.html"
+  },
+  {
+    "revision": "8182b695564d594879166a4ba08c41dc",
+    "url": "/ds-cards/json.worker.js"
+  },
+  {
+    "revision": "83aa2d6c6acaedd6575b",
+    "url": "/ds-cards/static/css/2.15516355.chunk.css"
+  },
+  {
+    "revision": "5e3dc9220180bd7e8de7",
+    "url": "/ds-cards/static/css/main.047dc825.chunk.css"
+  },
+  {
+    "revision": "83aa2d6c6acaedd6575b",
+    "url": "/ds-cards/static/js/2.5737f273.chunk.js"
+  },
+  {
+    "revision": "1f343f3b00bd42841cd23cdb6e2b08ec",
+    "url": "/ds-cards/static/js/2.5737f273.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49b3b530e9525a6aec74",
+    "url": "/ds-cards/static/js/3.095bbafe.chunk.js"
+  },
+  {
+    "revision": "5e3dc9220180bd7e8de7",
+    "url": "/ds-cards/static/js/main.d525c6d4.chunk.js"
+  },
+  {
+    "revision": "b9058e139205f6bd4959",
+    "url": "/ds-cards/static/js/runtime-main.14889c67.js"
+  },
+  {
+    "revision": "a609dc0f334a7d4e64205247c4e8b97c",
+    "url": "/ds-cards/static/media/codicon.a609dc0f.ttf"
+  }
+]);
